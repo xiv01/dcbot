3d41933a9d1686ae8203c52b7d341715f7fe4612
Max <killerhd176@gmail.com>
2022-08-21 22:33:21 +0200

    added github command

---
+++ commands/github.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('github')
+		.setDescription('links to this bots public github repo'),
+	async execute(interaction) {
+		const githubembed = new EmbedBuilder()
+			.setColor(0x6bfa94)
+			.setTitle('GitHub Repository')
+            .setURL('https://github.com/xiv01/dcbot/tree/main')
+		await interaction.reply({ embeds: [githubembed] });
+	},
+};
