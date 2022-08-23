249e4f1e0be5d7d2706e0433f1efa8fdf285affc
Max <killerhd176@gmail.com>
2022-08-23 06:43:52 +0200

    updated help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const helpembed = new EmbedBuilder()
     .setColor(0x6bfa94)
     .setTitle('🛠️ **Help**')
-    .setDescription('**/help**\n\`display commands summary\`\n\n**/github**\n\`links to this bots public github repo\`\n\n**/ping**\n\`displays bot & api latency\`\n\n**/rules**\n\`post server rules\`\n\n**/server**\n\`displays server information\`\n\n**/penis**\n\`calculates your pp size\`\n\n**/howgay**\n\`calculates your gayness\`\n\n**/setuproles**\n\`setup self roles channel\`\n᲼')
+    .setDescription('**/help**\n\`display commands summary\`\n\n**/catpic & /capypic**\n\`posts a cat / capybara pic\`\n\n**/mute & /unmute [member]**\n\`un-mute a member\`\n\n**/clear [messages]**\n\`delete given amount of messages\`\n\n**/github**\n\`links to this bots public github repo\`\n\n**/ping**\n\`displays bot & api latency\`\n\n**/rules**\n\`post server rules\`\n\n**/server**\n\`displays server information\`\n\n**/penis**\n\`calculates your pp size\`\n\n**/howgay**\n\`calculates your gayness\`\n\n**/setuproles**\n\`setup self roles channel\`\n᲼')
 	.setTimestamp()
 	.setFooter({ text: 'developed by max#0135', iconURL: 'https://cdn.discordapp.com/avatars/709098824253177859/b02b839c3cb08a00bd7354bc8afda92a.webp?size=32' });
 
