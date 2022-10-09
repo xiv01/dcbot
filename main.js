6918d8a554b4716df4cdda140ffe877d6a3fba62
Max <killerhd176@gmail.com>
2022-10-09 05:55:47 +0200

    :|

--- main.js
+++ main.js
@@ -89,8 +89,8 @@ client.on('messageCreate', async message => {
 
     if(message.channelId === bumpchannel) {
         message.embeds.forEach((embed) => {
+            console.log(embed.description)
             if(embed.description.includes("Bump erfolgreich!")) {
-                console.log(`[${[date.toLocaleString('en-US', { timeZone: 'Europe/Berlin' })]}] ${message.member.user.username}#${message.member.user.discriminator} bumped the server`)
                 const pingRole = message.guild.roles.cache.find(role => role.name === 'bumper');
                 setTimeout(() => message.channel.send(`${pingRole} bumpt ihr loser`), 7200000);
             }
