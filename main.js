ec5e3f730b460276d4ca67c43e1aa9d8dd31a75e
max <killerhd176@gmail.com>
2022-11-02 08:32:34 +0100

    more shrek pics

--- main.js
+++ main.js
@@ -108,7 +108,53 @@ client.on('messageCreate', async message => {
     }
 
     if(content.includes("shrek")) {
-        await message.reply('https://cdn140.picsart.com/298141702027211.png');
+        switch(Math.floor(Math.random() * 15) + 1) {
+            case 1:
+                await message.reply('https://cdn140.picsart.com/298141702027211.png');
+                break;
+            case 2:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037261342014447637/f2017f595b5c02a5049ea9b30acdd948.jpg');
+                break;
+            case 3:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037261341834100777/3bd4b1a8e7f1a139e1784c084a045ee4.jpg');
+                break;
+            case 4:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037261147402944602/5b6cb262e7ac3b01040762e4da468ffb.jpg');
+                break;
+            case 5:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037261007367712778/399425c53840b8b7d583cc7792e020fb.jpg');
+                break;
+            case 6:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260867458301995/12d6e1ce98f4fd77e35addab22dcb784.jpg');
+                break;
+            case 7:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260867231817759/5088dc5a21bd9791e34f557e4d2e3568.jpg');
+                break;
+            case 8:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260866967580702/601c0ca349b3c229917b8b16612acf6a.jpg');
+                break;
+            case 9:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260866573303818/177b4459eec137ae4865ba44a1089246.jpg');
+                break;
+            case 10:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260841340375050/a701b416f376c51673ffe421d2b2d7b0.jpg');
+                break;
+            case 11:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260841080340520/34fa091225dcc2a5910e0226d82b5fc5.jpg');
+                break;
+            case 12:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260840816095262/6abbb008e43ba05e4ec6c77dfb97a9c2.jpg');
+                break;
+            case 13:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260811741179944/09817db493156d8d76d11e29cffc1b33.jpg');
+                break;
+            case 14:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260811384668201/8d3c48dafd0558bc7eb58addc36d35ec.jpg');
+                break;
+            case 15:
+                await message.reply('https://cdn.discordapp.com/attachments/1037260765809348649/1037260810965221406/9291aaf3e0ef4c21829cf5597cf65771.jpg');
+                break;
+        }
     }
 
     if(content.includes(":catvibe:")) {
